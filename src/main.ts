5852b35496cc4e4307659cca22e3ec6b63920e69
Yoojeong Lee <yjclarelee@p4ace.com>
2021-08-04 19:22:45 +0900

    Create electron main file

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { app, BrowserWindow } from "electron"
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, 
+      contextIsolation: false
+    },
+  })
+  mainWindow.loadFile(`index.html`)
+}
+
+app.whenReady().then(createWindow)
